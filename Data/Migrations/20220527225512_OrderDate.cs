8df83e3ee5913788e2982b7a6d4fb2820b2a03bc
klevis.mema <klevis.mema@fshnstudent.info>
2022-05-28 01:22:49 +0200

    Ndryshime

---
+++ Data/Migrations/20220527225512_OrderDate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlyBuy.Data.Migrations
+{
+    public partial class OrderDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedDate",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Orders");
+        }
+    }
+}
